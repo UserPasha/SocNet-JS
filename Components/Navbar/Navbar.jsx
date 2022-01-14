b7c128a0f589ce690c6a8751a0a4f7000b246dbb
UserPasha <pasharoma13@gmail.com>
2022-01-15 02:13:35 +0300

    add a little style

--- Components/Navbar/Navbar.jsx
+++ Components/Navbar/Navbar.jsx
@@ -1,24 +1,24 @@
 import c from "./Navbar.module.css"
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 function Navbar() {
     return (
         <nav className={c.nav}>
-            <div className={`${c.item} ${c.active}`}>
-                <Link to="/profile">Profile</Link>
+            <div className={c.item} /*className={`${c.item} ${c.active}`}*/>
+                <NavLink to="/profile" className={navData => navData.isActive ? c.active : c.item}>Profile</NavLink>
             </div>
             <div className={c.item}>
-                <Link to="/messages">Messages</Link>
+                <NavLink to="/messages" className={navData => navData.isActive ? c.active : c.item}>Messages</NavLink>
             </div>
             <div className={c.item}>
-                <Link to="/news" >News</Link>
+                <NavLink to="/news" className={navData => navData.isActive ? c.active : c.item}>News</NavLink>
             </div>
 
             <div className={c.item}>
-                <Link to="/music" >Music</Link>
+                <NavLink to="/music" className={navData => navData.isActive ? c.active : c.item}>Music</NavLink>
             </div>
             <div className={c.item}>
-                <Link to="/settings">Settings</Link>
+                <NavLink to="/settings" className={navData => navData.isActive ? c.active : c.item}>Settings</NavLink>
             </div>
         </nav>
     )
