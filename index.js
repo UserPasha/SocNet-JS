b8e3564a84fc04ba66bb1aef785161f2ab0305dc
UserPasha <pasharoma13@gmail.com>
2022-03-08 00:39:58 +0300

    less 35-36, without render.js

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-
+import stateR, {subscribe} from "./Redux/state";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {rerenderEntireTree} from "./render";
-import stateR from "./Redux/state";
+import {BrowserRouter} from "react-router-dom";
+import {addMessage, addPost, updateMessageText, updatePostText} from "./Redux/state";
 
+
+ let rerenderEntireTree = (stateR) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App stateA={stateR}
+                     addPost={addPost}
+                     addMessage={addMessage}
+                     updatePostText={updatePostText}
+                     updateMessageText={updateMessageText}
+                />
+            </BrowserRouter>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
 
 rerenderEntireTree(stateR);
+
+subscribe(rerenderEntireTree)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
