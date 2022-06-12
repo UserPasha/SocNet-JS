1dec47f59ffb6b1a40099610a5e997575c152e00
UserPasha <pasharoma13@gmail.com>
2022-06-12 15:04:23 +0300

    add a css Loader

--- common/Components/Preloader.js
+++ common/Components/Preloader.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PreloaderImage from './../images/PreLoader.svg'
+import c from "./Preloader.module.css"
 
 const Preloader = () => {
     return (
         <div>
-            <img src={PreloaderImage} alt={'Preloader Image'}/>
+            <div className={c.loader}>
+
+            </div>
+
+            {/*<img src={PreloaderImage} alt={'Preloader Image'}/>*/}
         </div>
     );
 };
